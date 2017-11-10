f801358bbfe8bae7a716351da8234c07948be0b8
Arturocv <arturocv16@gmail.com>
2017-11-10 17:39:28 -0600

    Envio de videos implementado

--- main.cpp
+++ main.cpp
@@ -1,23 +1,55 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+#include <fstream>
 #include <iostream>
 
-using namespace std;
+typedef unsigned char byte;
 
-const sf::IpAddress IP = "10.0.2.15";
+const sf::IpAddress IP = "127.0.0.1";
 const int PORT = 2000;
+sf::TcpSocket socket;
 
-int main(int argc, char const *argv[])
-{
-    string id;
-    cout << "Nombre de Usuario: " << endl;
-    cin >> id;
+int sendVideo(std::string path);
 
-    sf::TcpSocket socket;
-    socket.connect(IP,PORT);
+int main(){
+
+    socket.connect(IP, PORT);
 
     sf::Packet packet;
-    packet << id;
+    packet << "Arturocv";
     socket.send(packet);
+
+    sendVideo("test.mp4");
+
+    return 0;
+}
+
+int sendVideo(std::string path) {
+    sf::Packet actionPacket;
+    actionPacket << "save";
+    socket.send(actionPacket);
+
+    sf::Packet videoPacket;
+    std::ifstream file(path, std::ios::binary);
+
+    //Se verifica que el video haya sido cargado de manera existosa
+    if(!file.is_open()){
+        std::cout << "Couldn't open the binary file" << std::endl;
+        return -1;
+    }
+
+    //Se convierte el video a un string de <uchar>
+    std::vector<byte> vector((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    std::string vectorString(vector.begin(), vector.end());
+    file.close();
+
+    //Se envia el video
+    videoPacket << vectorString;
+    socket.send(videoPacket);
+
+    sf::Packet donePacket;
+    donePacket << "done";
+    socket.send(donePacket);
+
     return 0;
 }
